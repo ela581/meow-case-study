9d5832f4d543552f5c2cac725519094b257dbd34
Elanur TÜRE <ela.ture.7@gmail.com>
2026-07-02 17:50:18 +0300

    finall

--- deploy-project-fixed-v2/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ContentPipeline from './ContentPipeline.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ContentPipeline />
-  </React.StrictMode>,
-)
